b885579ac9f4728f6fd1e855640385f446691380
Natalia Kulas <nataliakulas@gmail.com>
2022-08-22 13:27:30 +0200

    Pragma version updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: "0.8.11",
 };
 
 export default config;
