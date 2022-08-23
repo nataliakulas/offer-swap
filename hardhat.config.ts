93d8fc2b2c9af7cffc7f7df11201d505e4e63255
Natalia Kulas <nataliakulas@gmail.com>
2022-08-23 19:50:01 +0200

    Added gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.11",
+  gasReporter: {
+    enabled: true,
+  },
 };
 
 export default config;
