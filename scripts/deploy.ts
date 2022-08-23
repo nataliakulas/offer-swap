afc5fdaa0c5a0643937952ff22fa5da3a2c40a14
Natalia Kulas <nataliakulas@gmail.com>
2022-08-23 20:02:30 +0200

    Deploy script enhanced

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,21 @@
-import hre from "hardhat";
+import { ethers } from "hardhat";
 
 async function main() {
-  const accounts = await hre.ethers.provider.listAccounts();
+  const [seller, buyer] = await ethers.getSigners();
 
-  const CRMToken = await hre.ethers.getContractFactory("CRMToken");
+  const CRMToken = await ethers.getContractFactory("CRMToken");
   console.log("Deploying CRMToken...");
-  const cream = await CRMToken.deploy(accounts[1], 100);
+  const cream = await CRMToken.deploy(buyer.address, 100);
   await cream.deployed();
   console.log("CRMToken deployed to:", cream.address);
 
-  const WWTToken = await hre.ethers.getContractFactory("WWTToken");
+  const WWTToken = await ethers.getContractFactory("WWTToken");
   console.log("Deploying WWTToken...");
-  const wwt = await WWTToken.deploy(accounts[0]);
+  const wwt = await WWTToken.deploy(seller.address);
   await wwt.deployed();
   console.log("WWTToken deployed to:", wwt.address);
 
-  const Offer = await hre.ethers.getContractFactory("Offer");
+  const Offer = await ethers.getContractFactory("Offer");
   console.log("Deploying Offer...");
   const offer = await Offer.deploy(wwt.address, "1", cream.address, 1);
   await offer.deployed();
