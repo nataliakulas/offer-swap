7297fc4e7c52d4b4ae58c5693085978a5a463dff
Natalia Kulas <nataliakulas@gmail.com>
2022-08-23 19:34:48 +0200

    Offer contract changed according to comments

--- contracts/Offer.sol
+++ contracts/Offer.sol
@@ -3,49 +3,52 @@ pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Offer is Ownable {
-    IERC721 public ERC721;
-    IERC20 public ERC20;
+    IERC721 public immutable erc721;
+    using SafeERC20 for IERC20;
+    IERC20 public immutable erc20;
     uint256 public tokenId;
     uint256 public amount;
 
-    event Bought(uint256 tokenId, uint256 amount);
+    event Bought(
+        address indexed seller,
+        address indexed buyer,
+        address _erc721,
+        uint256 _tokenId,
+        address _erc20,
+        uint256 _amount
+    );
 
     constructor(
-        address _ERC721,
+        address _erc721,
         uint256 _tokenId,
-        address _ERC20,
+        address _erc20,
         uint256 _amount
     ) {
-        ERC721 = IERC721(_ERC721);
+        erc721 = IERC721(_erc721);
         tokenId = _tokenId;
-        ERC20 = IERC20(_ERC20);
+        erc20 = IERC20(_erc20);
         amount = _amount;
     }
 
-    function swap(
-        uint256 tokenId,
-        uint256 amount,
-        address seller
-    ) public {
-        require(seller == ERC721.ownerOf(tokenId), "Seller is not the owner");
-        require(
-            ERC721.getApproved(tokenId) == address(this),
-            "ERC721 is not approved"
-        );
-        require(ERC20.balanceOf(msg.sender) >= amount, "Not enough funds");
+    function swap() external {
+        address seller = erc721.ownerOf(tokenId);
 
-        ERC20.approve(address(this), amount);
-        require(
-            ERC20.allowance(msg.sender, address(this)) >= amount,
-            "ERC20 is not approved"
+        emit Bought(
+            seller,
+            msg.sender,
+            address(erc721),
+            tokenId,
+            address(erc20),
+            amount
         );
 
-        ERC721.safeTransferFrom(seller, msg.sender, tokenId);
-        ERC20.transfer(seller, amount);
+        erc721.safeTransferFrom(seller, msg.sender, tokenId);
+        erc20.safeTransferFrom(msg.sender, seller, amount);
 
-        emit Bought(tokenId, amount);
+        selfdestruct(payable(msg.sender));
     }
 }
